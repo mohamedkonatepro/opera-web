f35ca944535d4756c947df6ee636693a5adf80d2
Vincent METTON <vincent.metton@thetribe.io>
2023-03-03 16:46:25 +0100

    feat: show edl modalities

--- packages/tenant-appointment-booker/src/components/appointment-summary/AppointmentModalities.tsx
+++ packages/tenant-appointment-booker/src/components/appointment-summary/AppointmentModalities.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@mui/material";
+import { ArrowRight } from "@mui/icons-material";
+import { List, ListItem, ListItemIcon, ListItemText, Stack, Typography } from "@mui/material";
 
 const modalities = {
   EDL: [
@@ -10,11 +11,25 @@ const modalities = {
     "Si vous vous faites représenter, remettez à votre représentant une procuration manuscrite accompagnée d’une photocopie de votre pièce d’identité.",
     "Présentez à l’opérateur le certificat d’entretien de votre chaudière gaz si le logement est équipé d‘une chaudière de ce type.",
     "Pensez à apporter lors du rendez-vous, un exemplaire du bail et votre attestation d’assurance habitation.",
-  ],
+  ]
 };
 
 const AppointmentModalities = () => {
-  return <Box />;
+  return (
+    <Stack spacing={1.5}>
+      <Typography variant="caption" color="text.secondary">Le jour de l’état des lieux</Typography>
+      <List>
+        {modalities.EDL.map((modality) => (
+          <ListItem key={modality}>
+            <ListItemIcon>
+              <ArrowRight  sx={{ color: "text.disabled" }} />
+            </ListItemIcon>
+            <ListItemText primary={modality} />
+          </ListItem>
+        ))}
+      </List>
+    </Stack>
+  );
 };
 
 export default AppointmentModalities;
