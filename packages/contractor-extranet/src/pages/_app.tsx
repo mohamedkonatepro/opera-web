ec0d96642a017e8c5b682caab25f9ed4578a6ab4
Mohamed Konate <90257961+mohamedknt@users.noreply.github.com>
2023-04-13 14:43:37 +0200

    edit file review

--- packages/contractor-extranet/src/pages/_app.tsx
+++ packages/contractor-extranet/src/pages/_app.tsx
@@ -1,11 +1,65 @@
-import type { AppProps } from 'next/app'
-import theme from "../styles/theme";
+import * as React from "react";
+import Head from "next/head";
+import { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material/styles";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import theme from "../styles/theme";
+import createEmotionCache from "../styles/createEmotionCache";
+import { Container, Stack } from "@mui/material";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { Settings } from "luxon";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon";
 
-export default function App({ Component, pageProps }: AppProps) {
+Settings.defaultLocale = "fr";
+Settings.defaultZone = "Europe/Paris";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: { queries: { refetchOnWindowFocus: false } },
+      })
+  );
+
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <LocalizationProvider dateAdapter={AdapterLuxon} adapterLocale="fr">
+          <Stack component="main" height={1} justifyContent="space-between">
+            <Container
+              sx={{
+                maxWidth: { sm: "100%" },
+              }}
+              fixed
+              disableGutters
+            >
+              <QueryClientProvider client={queryClient}>
+                <Hydrate state={pageProps.dehydratedState}>
+                  <Component {...pageProps} />
+                </Hydrate>
+                <ReactQueryDevtools initialIsOpen={false} />
+              </QueryClientProvider>
+            </Container>
+          </Stack>
+        </LocalizationProvider>
+      </CacheProvider>
     </ThemeProvider>
-  )
+  );
 }
