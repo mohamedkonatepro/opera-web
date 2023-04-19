a3732d3fa58fcc606c0ae2cf789354ffb6ea3140
Vincent METTON <vincent.metton@thetribe.io>
2023-04-19 09:43:06 +0200

    feat: add api call to heating types

---
+++ packages/contractor-extranet/src/pages/api/heating-types/index.ts
@@ -0,0 +1,28 @@
+import { apiAxiosInstance } from "@/apiUtils/axiosInstance";
+import corsMiddleware, { cors } from "@/apiUtils/corsMiddleware";
+import handleError from "@/apiUtils/handleError";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await corsMiddleware(req, res, cors);
+  if (req.method === "GET") {
+    try {
+      const response = await apiAxiosInstance.get(
+        `/heating-types`
+      );
+      return res.status(200).json(
+        response.data.data.map((heatingType: any) => ({
+          id: heatingType.id,
+          name: heatingType.attributes.name,
+          code: heatingType.attributes.code,
+        }))
+      );
+    } catch (error: any) {
+      return handleError(error, res);
+    }
+  }
+
+  return res.status(405).json({ error: "Method not allowed" });
+};
+
+export default handler;
