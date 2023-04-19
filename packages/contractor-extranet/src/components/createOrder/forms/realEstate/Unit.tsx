391eef3ede309ceb9b562975f522a23d674eecfb
Vincent METTON <vincent.metton@thetribe.io>
2023-04-19 11:33:53 +0200

    feat: add inputs for unit reference

---
+++ packages/contractor-extranet/src/components/createOrder/forms/realEstate/Unit.tsx
@@ -0,0 +1,38 @@
+import { Stack, TextField, Typography } from "@mui/material";
+
+const Unit = () => {
+  return (
+    <Stack spacing={2}>
+      <Typography variant="subtitle1">Informations du lot</Typography>
+      <Stack spacing={2} direction="row">
+        <TextField
+          label="Référence immeuble"
+          color="secondary"
+          fullWidth
+        />
+        <TextField
+          label="N° de lot"
+          color="secondary"
+          fullWidth
+        />
+        <TextField
+          label="Mandat"
+          color="secondary"
+          fullWidth
+        />
+        <TextField
+          label="Code bail"
+          color="secondary"
+          fullWidth
+        />
+        <TextField
+          label="Année construction"
+          color="secondary"
+          fullWidth
+        />
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Unit;
--- packages/contractor-extranet/src/components/createOrder/forms/realEstate/index.tsx
+++ packages/contractor-extranet/src/components/createOrder/forms/realEstate/index.tsx
@@ -4,12 +4,14 @@ import RealEstate from "./RealEstate";
 import Annexes from "./Annexes";
 import Energy from "./Energy";
 import MeterLocation from "./MeterLocation";
+import Unit from "./Unit";
 
 const RealEstateForm = () => {
   return (
     <Stack spacing={5}>
       <Address />
       <RealEstate />
+      <Unit />
       <Annexes />
       <Energy />
       <MeterLocation />
