13223256401384eaa4ba3c4519d9f97e650085a6
Vincent METTON <vincent.metton@thetribe.io>
2023-05-03 16:34:03 +0200

    feat: hide contact form if tenant request

--- packages/tenant-appointment-booker/src/types/appointmentBooking.ts
+++ packages/tenant-appointment-booker/src/types/appointmentBooking.ts
@@ -1,11 +1,13 @@
 import Order from "./order";
 import Appointment from "./appointment";
+import TenantRequestProps from "./tenantResquestProps";
 
 interface AppointmentBooking {
   order: Order;
   id: string;
   appointment?: Appointment;
   uuid: string;
+  tenant_request?: TenantRequestProps;
 }
 
 export default AppointmentBooking;
