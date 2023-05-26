19509d0995a0f23cc7a86f846d01e8ceb43089dd
Vincent METTON <vincent.metton@thetribe.io>
2023-05-26 12:05:45 +0200

    feat: reorder services based on position

--- packages/contractor-extranet/src/middleware.ts
+++ packages/contractor-extranet/src/middleware.ts
@@ -1,18 +1,19 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 console.log('middleware', process.env.NEXT_PUBLIC_OG_EXTRANET_URL)
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/order')) {
-    const id = request.nextUrl.pathname.split('/')[2];
-    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_OG_EXTRANET_URL}/ordremission.asp?numcom=${id}`);
+  if (request.nextUrl.pathname.startsWith("/order")) {
+    const id = request.nextUrl.searchParams.get("id");
+    return NextResponse.redirect(
+      `${process.env.NEXT_PUBLIC_OG_EXTRANET_URL}/ordremission.asp?numcom=${id}`
+    );
   }
 
   return NextResponse.next();
-
 }
 
 export const config = {
-  matcher: ["/order/:id*"],
+  matcher: "/order/:id",
 };
--- packages/contractor-extranet/src/types/Service.ts
+++ packages/contractor-extranet/src/types/Service.ts
@@ -5,6 +5,7 @@ export interface Service {
   id: number;
   name: string;
   code: string;
+  position: number;
   item_reference: string;
   options: ServiceOption[];
   serviceTypes: ServiceType[];
