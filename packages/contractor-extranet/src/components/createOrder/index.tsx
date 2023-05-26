7d39e7abf40c6b7e965559580839d01d4aa6664a
Vincent METTON <metton.vincent@gmail.com>
2023-05-26 17:13:06 +0200

    Revert "fix: fix redirect order"

--- packages/contractor-extranet/src/components/createOrder/index.tsx
+++ packages/contractor-extranet/src/components/createOrder/index.tsx
@@ -46,11 +46,11 @@ const CreateOrderStepper: React.FC<{
 
   useEffect(() => {
     setStepStates(
-      getInitialValues(contractor, realEstate, {
+      getInitialValues(userContext.user.contractor, realEstate, {
         serviceTypes,
       })
     );
-  }, [contractor, realEstate, serviceTypes]);
+  }, [userContext.user.contractor, realEstate, serviceTypes]);
 
   const [needEstimateDialogOpen, setNeedEstimateDialogOpen] = useState(false);
 
