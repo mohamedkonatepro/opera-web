b4f58f8f05385553568b2b51245f012303a1dab6
Mohamed Konate <90257961+mohamedknt@users.noreply.github.com>
2023-05-29 16:32:54 +0200

    fix redirect

--- packages/contractor-extranet/src/middleware.ts
+++ packages/contractor-extranet/src/middleware.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-console.log('middleware', process.env.NEXT_PUBLIC_OG_EXTRANET_URL)
-
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/order")) {
-    const id = request.nextUrl.searchParams.get("id");
+    const id = request.nextUrl.pathname.split('/')[2];
     return NextResponse.redirect(
       `${process.env.NEXT_PUBLIC_OG_EXTRANET_URL}/ordremission.asp?numcom=${id}`
     );
@@ -15,5 +13,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: "/order/:id",
+  matcher: ["/order/:id*"],
 };
